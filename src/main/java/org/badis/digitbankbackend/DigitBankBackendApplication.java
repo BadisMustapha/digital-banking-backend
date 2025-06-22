3a5de0c623b54affd689aa7900cb30631f94760c
Mustapha Badis <mbadis@azura-group.com>
2025-06-22 12:44:28 +0100

    init projet

---
+++ src/main/java/org/badis/digitbankbackend/DigitBankBackendApplication.java
@@ -0,0 +1,13 @@
+package org.badis.digitbankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitBankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitBankBackendApplication.class, args);
+    }
+
+}
