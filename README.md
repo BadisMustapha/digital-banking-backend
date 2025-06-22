c46698dea1c89153bbdf59b7251ad1be2be83e06
Mustapha Badis <mbadis@azura-group.com>
2025-06-22 20:13:59 +0100

    Tester la couche DAO

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
+# Projet Spring Boot - Gestion de Comptes Bancaires
 
-1 - Créer un projet Spring Boot <br>
-![projet-init.png](img/projet-init.png)<br>
+## 1. Créer un projet Spring Boot
+![projet-init.png](img/projet-init.png)
 
-2. Créer les entités JPA : Customer, BankAccount, Saving Account, CurrentAccount, AccountOperation
-![entities.png](img/entities.png)<br>
+## 2. Créer les entités JPA
 
-3. Créer les interfaces JPA Repository basées sur Spring Data <br><br>
-![repositories.png](img/repositories.png) <br>
+- `Customer`
+- `BankAccount`
+- `SavingAccount`
+- `CurrentAccount`
+- `AccountOperation`
+
+![entities.png](img/entities.png)
+
+## 3. Créer les interfaces JPA Repository basées sur Spring Data
+
+![repositories.png](img/repositories.png)
+
+## 4. Tester la couche DAO
+
+### Résultat des tests
+
+- Test DAO général :
+
+  ![testDAO.png](img/testDAO.png)
+
+- Création d’un client (`Customer`) :
+
+  ![customer.png](img/customer.png)
+
+- Création d’un compte bancaire (`BankAccount`) :
+
+  ![bankAccount.png](img/bankAccount.png)
+
+- Création des operations d’un compte bancaire (`AccountOperation`) :
+
+  ![accountOperation.png](img/accountOperations.png)
--- src/main/java/org/badis/digitbankbackend/DigitBankBackendApplication.java
+++ src/main/java/org/badis/digitbankbackend/DigitBankBackendApplication.java
@@ -1,13 +1,75 @@
 package org.badis.digitbankbackend;
 
+import org.badis.digitbankbackend.entities.AccountOperation;
+import org.badis.digitbankbackend.entities.CurrentAccount;
+import org.badis.digitbankbackend.entities.Customer;
+import org.badis.digitbankbackend.entities.SavingAccount;
+import org.badis.digitbankbackend.enums.AccountStatus;
+import org.badis.digitbankbackend.enums.OperationType;
+import org.badis.digitbankbackend.repositories.AccountOperationRepository;
+import org.badis.digitbankbackend.repositories.BankAccountRepository;
+import org.badis.digitbankbackend.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DigitBankBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DigitBankBackendApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepository){
+        return args -> {
+            Stream.of("Mustapha","Noureddine","Maryam").forEach(name->{
+                Customer customer=new Customer();
+                customer.setName(name);
+                customer.setEmail(name+"@yopmail.com");
+                customerRepository.save(customer);
+            });
+
+            customerRepository.findAll().forEach(cust->{
+                CurrentAccount currentAccount=new CurrentAccount();
+                currentAccount.setId(UUID.randomUUID().toString());
+                currentAccount.setBalance(Math.random()*30000);
+                currentAccount.setCreatedAt(new Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setCustomer(cust);
+                currentAccount.setOverdraft(5000);
+                bankAccountRepository.save(currentAccount);
+
+                SavingAccount savingAccount=new SavingAccount();
+                savingAccount.setId(UUID.randomUUID().toString());
+                savingAccount.setBalance(Math.random()*30000);
+                savingAccount.setCreatedAt(new Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setCustomer(cust);
+                savingAccount.setInterestRate(4.7);
+                bankAccountRepository.save(savingAccount);
+
+            });
+            bankAccountRepository.findAll().forEach(acc->{
+                for (int i = 0; i <10 ; i++) {
+                    AccountOperation accountOperation=new AccountOperation();
+                    accountOperation.setOperationDate(new Date());
+                    accountOperation.setAmount(Math.random()*12000);
+                    accountOperation.setType(Math.random()>0.5? OperationType.DEBIT: OperationType.CREDIT);
+                    accountOperation.setBankAccount(acc);
+                    accountOperationRepository.save(accountOperation);
+                }
+
+            });
+        };
+
     }
 
 }
